2adf3c8474c80c30f0f2076a80ef6484e723dd8c
Alex Mendoza <amendoza@lya-electronic.com>
2021-11-30 14:34:26 -0500

    punto 20

---
+++ punto20/punto20.py
@@ -0,0 +1,23 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+url = 'covid_22_noviembre.csv'
+data = pd.read_csv(url)
+
+data.rename(columns={'ID de caso': 'ID'}, inplace=True)
+# tratando los datos
+data['Ubicación del caso'].replace('CASA', 'Casa', inplace=True)
+data['Recuperado'].replace('fallecido', 'Fallecido', inplace=True)
+data['Tipo de contagio'].replace('relacionado', 'Relacionado', inplace=True)
+data['Tipo de contagio'].replace('RELACIONADO', 'Relacionado', inplace=True)
+data['Tipo de contagio'].replace('EN ESTUDIO', 'En estudio', inplace=True)
+data['Tipo de contagio'].replace('En Estudio', 'En estudio', inplace=True)
+data['Sexo'].replace('m', 'M', inplace=True)
+data['Sexo'].replace('f', 'F', inplace=True)
+
+
+
+print('\n20)')
+pais_procedencia = data.groupby('Nombre del país').count()
+print(pais_procedencia['ID'].sort_values(ascending=False))
