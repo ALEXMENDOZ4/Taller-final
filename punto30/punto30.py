05216767bf5f3822cc2da54e79f52709009ecab7
Alex Mendoza <amendoza@lya-electronic.com>
2021-11-30 15:06:32 -0500

    punto 30

---
+++ punto30/punto30.py
@@ -0,0 +1,22 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+url = 'covid_22_noviembre.csv'
+data = pd.read_csv(url)
+
+data.rename(columns={'ID de caso': 'ID'}, inplace=True)
+# tratando los datos
+data['Ubicación del caso'].replace('CASA', 'Casa', inplace=True)
+data['Recuperado'].replace('fallecido', 'Fallecido', inplace=True)
+data['Tipo de contagio'].replace('relacionado', 'Relacionado', inplace=True)
+data['Tipo de contagio'].replace('RELACIONADO', 'Relacionado', inplace=True)
+data['Tipo de contagio'].replace('EN ESTUDIO', 'En estudio', inplace=True)
+data['Tipo de contagio'].replace('En Estudio', 'En estudio', inplace=True)
+data['Sexo'].replace('m', 'M', inplace=True)
+data['Sexo'].replace('f', 'F', inplace=True)
+
+
+
+print('\n30)')
+print(data.groupby('Edad')['ID'].count().sort_values(ascending=False))
