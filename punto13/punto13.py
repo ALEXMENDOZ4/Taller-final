996ab0541dc2b74b8930b780335e8f1c7e536e37
Alex Mendoza <amendoza@lya-electronic.com>
2021-11-30 14:26:10 -0500

    punto 13

---
+++ punto13/punto13.py
@@ -0,0 +1,23 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+url = 'covid_22_noviembre.csv'
+data = pd.read_csv(url)
+
+data.rename(columns={'ID de caso': 'ID'}, inplace=True)
+# tratando los datos
+data['Ubicación del caso'].replace('CASA', 'Casa', inplace=True)
+data['Recuperado'].replace('fallecido', 'Fallecido', inplace=True)
+data['Tipo de contagio'].replace('relacionado', 'Relacionado', inplace=True)
+data['Tipo de contagio'].replace('RELACIONADO', 'Relacionado', inplace=True)
+data['Tipo de contagio'].replace('EN ESTUDIO', 'En estudio', inplace=True)
+data['Tipo de contagio'].replace('En Estudio', 'En estudio', inplace=True)
+data['Sexo'].replace('m', 'M', inplace=True)
+data['Sexo'].replace('f', 'F', inplace=True)
+
+
+recuperados = data[data['Recuperado'] == 'Recuperado']
+print('\n13)')
+dep_recu = recuperados.groupby('Nombre departamento').count()
+print(dep_recu['ID'].sort_values(ascending=False).head(10))
