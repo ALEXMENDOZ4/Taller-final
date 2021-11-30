d540386c49d16ed04321ab64d6371e5f82fdcbd0
Alex Mendoza <amendoza@lya-electronic.com>
2021-11-30 14:05:29 -0500

    punto 3

---
+++ punto3/punto3.py
@@ -0,0 +1,21 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+url = 'covid_22_noviembre.csv'
+data = pd.read_csv(url)
+
+data.rename(columns={'ID de caso': 'ID'}, inplace=True)
+# tratando los datos
+data['Ubicación del caso'].replace('CASA', 'Casa', inplace=True)
+data['Recuperado'].replace('fallecido', 'Fallecido', inplace=True)
+data['Tipo de contagio'].replace('relacionado', 'Relacionado', inplace=True)
+data['Tipo de contagio'].replace('RELACIONADO', 'Relacionado', inplace=True)
+data['Tipo de contagio'].replace('EN ESTUDIO', 'En estudio', inplace=True)
+data['Tipo de contagio'].replace('En Estudio', 'En estudio', inplace=True)
+data['Sexo'].replace('m', 'M', inplace=True)
+data['Sexo'].replace('f', 'F', inplace=True)
+
+municipios = data['Nombre municipio'].unique()
+print('\n3)')
+print('municipios afectados: \n' + str(municipios))
